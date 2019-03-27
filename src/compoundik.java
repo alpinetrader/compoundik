d23e8f68f909c3512f8c0f325587400c68f3643e
alpinetrader <alpinetrader@gmail.com>
2019-03-27 16:24:43 +0200

    Initial commit 22

---
+++ src/compoundik.java
@@ -0,0 +1,22 @@
+/*
+1.2.24 Continuously compounded interest. Write a program that calculates and
+prints the amount of money you would have after t years if you invested P dollars
+at an annual interest rate r (compounded continuously). The desired value is given
+by the formula Pe rt.
+*/
+
+
+public class compoundik
+{
+    public static void main(String[] args)
+    {
+       int  t   = Integer.parseInt(args[0]);   // years
+       double P = Double.parseDouble(args[1]);  // dollars
+       double r = Double.parseDouble(args[2]);  // annual interest rate
+
+
+
+
+        System.out.println(P*Math.exp(r*t));
+    }
+}
